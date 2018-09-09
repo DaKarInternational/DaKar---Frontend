86286fba01592dd421e4c7cdf57c55e4968e4ec7
blablanumerodeux <blablanumerodeux@gmail.com>
2018-09-09 19:45:03 -0400

    features/56_first_use_case_create_journey

--- src/App.js
+++ src/App.js
@@ -8,17 +8,16 @@ import './App.css';
 
 class App extends Component {
 
-// Render the Application
-render() {
+  // Render the Application
+  render() {
 
-  return (
-    <div>
-      <Header/>
-      <Main/>
-    </div>
-  );
-}
-
+    return (
+      <div>
+        <Header/>
+        <Main/>
+      </div>
+    );
+  }
 }
 
 export default App;
